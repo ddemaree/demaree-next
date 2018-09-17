414c3d7c5fb5b29b51fb33b913002419a19a000f
David Demaree <david@demaree.me>
2018-09-17 23:55:39 +0000

    Add book section

--- pages/writing.js
+++ pages/writing.js
@@ -12,6 +12,18 @@ export default class extends React.Component {
     return (
     <Fragment>
       <h2 className="section-hed">Writing</h2>
+      
+      <figure className="book-section__hero">
+        <img src="//cdn.shopify.com/s/files/1/0051/7692/products/aba-000017-pp-3_660x@2x.progressive.jpg?v=1482334015" />
+      </figure>
+
+      <h3>Git for Humans</h3>
+      <blockquote>
+        <p>You can’t succeed in the modern tech industry without understanding Git workflows. Thankfully, now there’s a guide that makes Git genuinely accessible—and this is it. It’s wonderful to see the power of Git expressed in clear, straightforward terms. I strongly recommend this book to anyone who works in technology.</p>
+          <cite>Paul Ford</cite>
+      </blockquote>
+      
+      <h3>Articles on Medium</h3>
       {posts.map( post => (
       <article className="medium-post" key={post.link}>
           <h3 className="title"><a href={post.link}>{post.title}</a></h3>
