ea01d2214fae6f6bf4a90e7e1db2cae21ec818d5
David Demaree <david@demaree.me>
2018-09-22 19:14:27 -0400

    Start integrating WP blog content

--- pages/writing/post.js
+++ pages/writing/post.js
@@ -1,1 +1,22 @@
-export default () => <div><h1>tbd</h1></div>
+import React from 'react'
+import { getPostWithSlug } from '../../src/WordPress'
+
+export default class extends React.Component {
+  static async getInitialProps({ query }) {
+    let post = await getPostWithSlug(query.slug)
+    console.log(post)
+    return { post }
+  }
+
+  render() {
+    const { post } = this.props
+    return (
+      <article>
+        <header>
+          <h2 dangerouslySetInnerHTML={{ __html: post.title.rendered }} />
+          <section className="entry-content" dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+        </header>
+      </article>
+    )
+  }
+}
--- src/WordPress.js
+++ src/WordPress.js
@@ -1,23 +1,41 @@
 import axios from 'axios'
 import _forEach from 'lodash/forEach'
+import _pick from 'lodash/pick'
 
 const wpClient = axios.create({
-  baseURL: 'https://bitsandletters.club/wp-json/wp/v2'
+  baseURL: 'https://bitsandletters.club/wp-json'
 })
 
-export function transformPost(postData) {
-  let { guid, ...rawPost } = postData
+export function transformPost(postData, includeContent = false) {
+  let pickFields = ['guid', 'title', 'acf', 'excerpt', 'date_gmt', 'modified_gmt', 'slug', 'status', 'format', 'categories', 'tags']
 
-  return Object.extend({}, rawPost, {
-    guid: guid.rendered
+  if(includeContent) pickFields.push('content')
+
+  _forEach(postData, (value, key, coll) => {
+    if(key === 'guid') {
+      coll.guid = value.rendered
+    }
   })
+  return _pick(postData, pickFields)
 }
 
-export const getPosts = () => {
-  return wpClient.get('/posts')
-    .then(({ data }) => data)
+export const getPosts = (args = {}) => {
+  let { page, includeContent } = args
+  page = page || 0
+
+  return wpClient.get('/dd/v1/posts?context=edit')
+    .then(({ data }) => data.map(d => transformPost(d, !!includeContent)))
     .catch(error => {
       console.log(error)
       return []
+    })
+}
+
+export const getPostWithSlug = (slug) => {
+  return wpClient.get('/dd/v1/post?context=edit&slug=' + slug)
+    .then(({ data }) => transformPost(data, true))
+    .catch(error => {
+      console.log(error)
+      return {}
     })
 }
