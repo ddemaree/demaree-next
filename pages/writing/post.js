609be20d378aa7bd9e7c189364dc158965066fe0
David Demaree <david@demaree.me>
2018-09-23 07:47:57 -0400

    Check for slug param in writing/post

--- pages/writing/post.js
+++ pages/writing/post.js
@@ -1,22 +1,28 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { getPostWithSlug } from '../../src/WordPress'
 
 export default class extends React.Component {
   static async getInitialProps({ query }) {
-    let post = await getPostWithSlug(query.slug)
-    console.log(post)
-    return { post }
+    if(query.slug) {
+      let post = await getPostWithSlug(query.slug)
+      console.log(post)
+      return { post }
+    } else {
+      return {}
+    }
   }
 
   render() {
     const { post } = this.props
     return (
-      <article>
-        <header>
-          <h2 dangerouslySetInnerHTML={{ __html: post.title.rendered }} />
-          <section className="entry-content" dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
-        </header>
-      </article>
+      <Fragment>
+        {post && <article>
+          <header>
+            <h2 dangerouslySetInnerHTML={{ __html: post.title.rendered }} />
+            <section className="entry-content" dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+          </header>
+        </article>}
+      </Fragment>
     )
   }
 }
