c0fb3b85ef37034f30abc2e91c1af448b8e33c03
David Demaree <david@demaree.me>
2019-09-11 21:24:57 -0400

    Fix some configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,22 +4,20 @@ const path = require('path')
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `demaree.me`,
+    description: `David's web site`,
+    author: `@ddemaree`,
   },
   plugins: [
     {
       resolve: `gatsby-plugin-sass`,
       options: {
-        // implementation: require("sass"),
         includePaths: [
           path.resolve(__dirname, 'node_modules'),
           path.resolve(__dirname, 'src/sass_includes')
         ]
       },
     },
-    // `gatsby-plugin-emotion`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -30,18 +28,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
     {
       resolve: `gatsby-source-ghost`,
       options: {
