939bf6cac4d4263b640f6bf94527ea52232fbdcc
David Demaree <david@demaree.me>
2020-02-09 20:38:11 -0500

    Home page styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,16 @@ const IndexPage = ({ data }) => {
 
   return (
     <Layout>
+      <div class="relative dark-overlay home-header">
+
+        <div className="absolute z-10 w-full inset-y-0 flex flex-col justify-center">
+          <h1 className="font-display-serif text-6xl xs:text-5xl text-white text-center leading-none">Hi there.</h1>
+        </div>
+
+        <figure className="z-0 h-64 lg:h-verytall">
+          <img className="h-full w-full object-cover object-center" src="//res.cloudinary.com/demaree/image/upload/v1581295512/L2090706-2.jpg" />
+        </figure>
+      </div>
       <div className="my-8 mx-auto">
         <PageContent content={html} />
       </div>
