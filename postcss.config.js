e0304d425efe7768ca6d3e0b5f9e648136634a1b
David Demaree <david@demaree.me>
2020-04-27 20:25:42 -0400

    Clean up postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,3 @@
 module.exports = () => ({
-  plugins: [
-    require('postcss-import'),
-    require('postcss-url')([
-      { filter: '**/*.woff*', url: 'rebase' } 
-    ]),
-    require("tailwindcss")("./tailwind.config.js"),
-    require("postcss-nested"),
-    // require('postcss-custom-properties')({
-    //   importFrom: [
-    //     {
-    //       customProperties: { '--dd-hello-world': '#f0f3' }
-    //     }
-    //   ]
-    // }),
-  ],
-})
+	plugins: [require('postcss-import'), require('postcss-url')([{ filter: '**/*.woff*', url: 'rebase' }]), require('tailwindcss')('./tailwind.config.js'), require('postcss-nested')],
+});
