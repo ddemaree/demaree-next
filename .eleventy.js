7097ebafd689ff6314220aafd74000c992631d92
David Demaree <david@demaree.me>
2020-04-30 22:06:06 -0400

    Move fonts into 11ty src

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,18 @@
-module.exports = {
-	dir: {
-		input: 'src',
-	},
+module.exports = function(eleventyConfig) {
+	let markdownIt = require('markdown-it');
+	let markdownItAttrs = require('markdown-it-attrs');
+	let markdownItContainer = require('markdown-it-container');
+	let options = {
+		html: true,
+	};
+	let markdownLib = markdownIt(options)
+		.use(markdownItAttrs)
+		.use(markdownItContainer);
+	eleventyConfig.setLibrary('md', markdownLib);
+
+	return {
+		dir: {
+			input: 'src',
+		},
+	};
 };
--- src/index.md
+++ src/index.md
@@ -2,4 +2,7 @@
 layout: default
 title: David's home page
 ---
-# Hi. I'm David Demaree.
+
+# Hi. I'm David Demaree. {.home-title}
+
+Here is some bio text you may find interesting.
