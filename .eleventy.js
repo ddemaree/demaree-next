2d404e543dccb5573f7df6ff56945c2008e605c0
David Demaree <david@demaree.me>
2020-05-17 22:23:24 -0400

    Home page magic

--- .eleventy.js
+++ .eleventy.js
@@ -11,6 +11,7 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.setLibrary('md', markdownLib);
 
 	eleventyConfig.addPassthroughCopy("src/images");
+	eleventyConfig.addPassthroughCopy("src/icons");
 
 	return {
 		dir: {
