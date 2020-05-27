b2a74c0a237e26d61e061ff6dacb84e96db6a6ee
David Demaree <david@demaree.me>
2020-05-26 22:25:32 -0400

    Hero unit looking sharp

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,49 @@ import moment from 'moment'
 import styles from './index.module.css'
 import c from 'classnames'
 
+
+const AboutLink = ({ className }) =>
+  <p className={c(`mt-4`, className)}>
+    <a href="/about" className="inline-block leading-none p-3 bg-black text-white rounded-lg text-sm">More about me</a>
+  </p>
+
+const Hero = ({ imageSrc }) =>
+  <div className={`col-gap-6 row-gap-4 xs:grid grid-cols-12 items-center max-w-lg md:max-w-xl mx-auto`}>
+    <figure className="w-16 xs:w-full mb-4 col-span-3">
+      {imageSrc && <img alt="Cartoon portrait of David Demaree" src={imageSrc} className="max-w-full" />}
+    </figure>
+    <p className="text-2xl md:text-3xl leading-tight font-normal col-span-9"><span className="font-semibold">David&nbsp;Demaree</span> is a product&nbsp;manager, designer, and writer based in New&nbsp;Jersey</p>
+
+    <AboutLink className="col-start-4 col-span-9 xs:mt-0" />
+  </div>
+
+const sourceClasses = {
+  medium: 'fab fa-medium',
+  substack: 'far fa-envelope-open-text',
+  blog: 'fas fa-newspaper'
+}
+
+const LatestPostsItem = ({ post }) => {
+  return <article key={post.link}>
+    <a href={post.link} className="mb-2 pl-12 md:pl-0 relative block no-underline md:flex items-center">
+      <span className="w-8 text-ink-medium text-lg inline-block left-0 md:left-auto absolute md:static text-center">
+        <i className={c(sourceClasses[post.source])}></i>
+      </span>
+      <h3 className="underline flex-1 md:ml-4">{post.title}</h3>
+      <div className="text-sm text-ink-medium no-underline">
+        <time>{moment(post.date).format('MMM D, YYYY')}</time>
+      </div>
+    </a>
+  </article>
+}
+
 const IndexPage = ({ data }) => {
-  const { memoji, substackItems, mediumItems } = data
+  const { memoji, substackItems, mediumItems, ghostPosts } = data
 
   const allPostItems = _.concat(
     substackItems.edges.map(e => ({ source: 'substack', ...e.node })),
-    mediumItems.edges.map(e => ({ source: 'medium', ...e.node }))
+    mediumItems.edges.map(e => ({ source: 'medium', ...e.node })),
+    ghostPosts.edges.map(e => ({ source: 'blog', isoDate: e.node.published_at, link: `/${e.node.slug}`, ...e.node }))
   ).map(p => {
     const { isoDate, ...postData } = p
     const ts = Date.parse(isoDate)
@@ -26,26 +63,11 @@ const IndexPage = ({ data }) => {
   return (
     <Layout headerClassName="bg-container">
       <section className="bg-container">
-        <div className={c(["dd-wrap relative", styles.gridContainer])}>
+        <div className="dd-wrap min-h-95v md:min-h-75v box-border relative flex items-center pt-8 pb-4">
+          <Hero imageSrc={memoji.img.fixed.src} />
 
-          <figure className="w-24 mb-4">
-            {memoji && <img src={memoji.img.fixed.src} className="max-w-full" />}
-          </figure>
-          
-          <div className="text-2xl font-semibold leading-tight max-w-lg">
-            <p>David&nbsp;Demaree is a product&nbsp;manager, designer, and writer based in New&nbsp;Jersey, U.S.A.</p>
-
-            {/* <p className="text-4xl leading-none mb-6">Hi, I'm David. I love code, cameras, coffee, and cocktails. <span className="whitespace-no-wrap">(Also alliteration.)</span></p> */}
-
-            {/* <p>I live in the NYC suburbs with <a href="https://instagram.com/jdweinmann">my wife</a>, our 5-year-old daughter, and <a href="https://instagram.com/johnny.cash.frenchie">a cute French bulldog</a>. I've been making web sites and web software for 25 years, most recently leading design and typography teams at big companies. I'm also a published author and speak at conferences worldwide.</p> */}
-
-            <p className="mt-6">
-              <a href="/about" className="leading-none text-base p-3 bg-black text-white rounded-lg">More about me</a>
-            </p>
-          </div>
-
-          <div aria-hidden="true" className="absolute pb-2 text-center left-0 right-0 bottom-0">
-            <i className="fas fa-chevron-down"></i>
+          <div aria-hidden="true" className="pb-4 flex items-center justify-center absolute bottom-0 left-0 right-0">
+            <i className="fas fa-chevron-down text-ink-low"></i>
           </div>
 
         </div>
@@ -53,19 +75,8 @@ const IndexPage = ({ data }) => {
 
       <section className="py-8">
         <div className="dd-wrap">
-          <h2 className="text-2xl mb-4">Writings</h2>
-          {sortedPostItems.map(post => 
-            <article key={post.link}>
-              <a href={post.link} className="mb-2 pl-12 md:pl-0 relative block no-underline md:flex items-center">
-                <span className="w-8 text-ink-medium text-lg inline-block left-0 md:left-auto absolute md:static text-center">
-                  <i className={c([(post.source === 'medium' ? 'fab fa-medium' : 'fas fa-newspaper')])}></i>
-                </span>
-                <h3 className="underline flex-1 md:ml-4">{post.title}</h3>
-                <div className="text-sm text-ink-medium no-underline">
-                  <time>{moment(post.date).format('MMM D, YYYY')}</time>
-                </div>
-              </a>
-            </article>)}
+          <h2 className="text-2xl mb-4">Latest</h2>
+          {sortedPostItems.map(post => <LatestPostsItem post={post} />)}
         </div>
       </section>
     </Layout>
@@ -107,6 +118,16 @@ query HomepageQuery {
         content {
           encoded
         }
+      }
+    }
+  }
+  ghostPosts: allGhostPost {
+    edges {
+      node {
+        title
+        excerpt
+        published_at
+        slug
       }
     }
   }
