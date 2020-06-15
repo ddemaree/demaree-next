878046a640693034ea61baedf686fc3fa2b10b41
David Demaree <david@demaree.me>
2020-06-14 21:28:02 -0400

    Code cleanup in posts-list

--- src/components/posts-list.js
+++ src/components/posts-list.js
@@ -46,23 +46,16 @@ const processPostsData = data => {
   return _.sortBy(allPostItems, 'sortTimestamp').reverse()
 }
 
-const sourceClasses = {
-  medium: 'fab fa-medium',
-  substack: 'far fa-envelope-open-text',
-  blog: 'fas fa-newspaper',
-  tumblr: 'fab fa-tumblr'
-}
-
-const excerptify = (str, len = 12) => str.replace(/(<([^>]+)>)/ig,"").split(" ").slice(0,len).join(' ')
-
 const LatestPostsItem = ({ post }) => {
 
   return <article key={post.link}>
     <div className="mb-6 relative block no-underline sm:flex">
       <div className="flex-1 text-base">
-        <h3 className="dd-link-underline font-normal my-0"><Link to={post.link}>{post.title}</Link></h3>
+        <h3 className="dd-link-underline font-normal my-0">
+          <Link to={post.link}>{post.title}</Link>
+        </h3>
         <div className="max-w-md text-sm text-ink leading-snug no-underline">
-          {post.excerpt && `${excerptify(post.excerpt, 20)}`}
+          {post.excerpt}
         </div>
       </div>
       <p className="font-soehne text-sm text-ink-medium mt-1 md:mt-0 sm:order-first w-24 md:w-32">
