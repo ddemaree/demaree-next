9f86743d2b8dc773598cb16a61a94f70adddf7af
David Demaree <david@demaree.me>
2020-07-15 16:24:22 -0400

    Tweak debug values

--- src/pages/debug.js
+++ src/pages/debug.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
-export default () => {
-  const { DEPLOY_PRIME_URL } = process.env
+export default ({ data }) => {
+  const deployUrl = process.env.DEPLOY_PRIME_URL
   return <div>
-    Deploy URL: {DEPLOY_PRIME_URL || "None"}
+    <p>Deploy URL: {deployUrl || "Not set"}</p>
+    <p>Build URL from config: {data.site.siteMetadata.buildUrl || "Not set"}</p>
   </div>
 }
+
+export const query = graphql`
+  query HomePageQuery {
+    site {
+      siteMetadata {
+        buildUrl
+      }
+    }
+  }
+`
