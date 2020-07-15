35fd0adff0ec6104d66c9a3fb95209f4dfe84650
David Demaree <david@demaree.me>
2020-07-14 21:42:15 -0400

    Remove cruft

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,30 +51,6 @@ module.exports = {
       }
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-556801-2",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        // exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 100,
-      },
-    },
-    // {
-    //   resolve: `gatsby-source-tumblr`,
-    //   options: {
-    //     blogIdentifier: `ddemaree`,
-    //     consumerKey: `QPuKF2pR1MHhU75b7MK1QFSsJMdQcxvHL4ZO3sAfKUSmVCe2fw`
-    //   }
-    // },
+    `gatsby-plugin-sharp`
   ],
 }
