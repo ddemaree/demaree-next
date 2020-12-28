12c2490dab7d492873224b1f6f151b057831b003
David Demaree <david@demaree.me>
2020-12-27 19:36:51 -0500

    WIP integration

--- pages/wp/index.js
+++ pages/wp/index.js
@@ -4,6 +4,10 @@ const wpComAPI = axios.create({
   baseURL: "https://public-api.wordpress.com/rest/v1.1/sites/ddemaree.wordpress.com" 
 })
 
+const wpAPI = axios.create({
+  baseURL: "https://wp.demaree.space/wp-json/wp/v2"
+})
+
 function WordPressPosts({ posts }) {
   return <h1>Hello</h1>
 }
@@ -11,7 +15,12 @@ function WordPressPosts({ posts }) {
 export default WordPressPosts
 
 export async function getStaticProps({ params }) {
-  const posts = await wpComAPI.get("/posts").then(res => res.data)
+  const posts = await wpAPI.get("/posts", {
+    params: {
+      per_page: 100,
+      '_embed': 1
+    }
+  }).then(res => res.data)
   console.log(posts)
 
   return {
