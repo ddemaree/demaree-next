e7fc6dfc57aaec65566d01003bc893d0de9ac876
David Demaree <david@demaree.me>
2021-01-12 19:41:17 -0500

    WIP: Vue based sidebar menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,45 @@
-import "alpinejs"
+// import "alpinejs"
+
+import Vue from 'vue/dist/vue.esm'
+
+function getScrollbarWidth() {
+  return window.innerWidth - document.documentElement.clientWidth;
+}
+
+const scrollWidth = getScrollbarWidth()
+console.log("Scroll width is ", scrollWidth)
+
+function setBodyScroll(isScrollable = true){
+  console.log("Scroll width is ", scrollWidth)
+
+  if(isScrollable) {
+    document.body.style.paddingRight = '0px'
+    document.body.classList.remove('overflow-y-hidden')
+  } else {
+    document.body.style.paddingRight = `${scrollWidth}px`;
+    document.body.classList.add('overflow-y-hidden')
+  }
+}
+
+const menuVm = new Vue({
+  el: "#tha-menu",
+  data: {
+    open: false,
+    menuItems: (window.__menus || [])
+  },
+  created() {
+    if(this.open) {
+      setBodyScroll(false)
+    } else {
+      setBodyScroll(true)
+    }
+  }
+})
+
+menuVm.$watch('open', newVal => {
+  setBodyScroll(!newVal)
+})
+
 
 const config = {
   rootMargin: '0px 0px 0px 0px',
