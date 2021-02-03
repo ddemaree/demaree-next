5bc2d0872c42ebf9a2938927fcb703aca757ecc1
David Demaree <david@demaree.me>
2021-02-03 16:16:22 -0500

    Make syntax highlighting fancy

--- postcss.config.js
+++ postcss.config.js
@@ -1,31 +1,3 @@
-/*
-
-	plugins: [  require('tailwindcss')('./tailwind.config.js'), ],
-
-*/
-
-// module.exports = {
-//   plugins: [
-//     require('postcss-import'),
-//     require('postcss-url')([{ filter: '**/*.woff*', url: 'rebase' }]),
-//     require(`tailwindcss`)(`./tailwind.config.js`),
-//     require('postcss-nested'),
-//     require(`autoprefixer`),
-//     ...(process.env.ELEVENTY_PRODUCTION
-//       ? [
-//           require(`postcss-clean`),
-//           require(`@fullhuman/postcss-purgecss`)({
-//             content: ["_site/**/*.html"],
-//             defaultExtractor: (content) =>
-//               content.match(/[\w-/:]+(?<!:)/g) || [],
-//             whitelist: [],
-//             whitelistPatterns: [/body/, /headroom/, /ril/],
-//           }),
-//         ]
-//       : []),
-//   ],
-// };
-
 module.exports = {
   plugins: {
     'postcss-import': {},
