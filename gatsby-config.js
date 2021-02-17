6faa86f6f93a3bc1dc77dd5515265a5af5efbc48
David Demaree <david@demaree.me>
2021-02-17 09:35:48 -0500

    Experiment to add Gatsby to project

---
+++ gatsby-config.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+
+const path = require('path')
+
+const siteURL = (process.env.CONTEXT === "production" 
+  ? process.env.URL
+  : process.env.DEPLOY_PRIME_URL) || "localhost:9000";
+
+console.log(`Site URL is ${siteURL}`)
+
+module.exports = {
+  siteMetadata: {
+    title: `demaree.me`,
+    description: `David's web site`,
+    author: `@ddemaree`,
+    buildUrl: (siteURL || "")
+  },
+  plugins: [
+    'gatsby-plugin-postcss',
+    `gatsby-plugin-react-helmet`,
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: `${__dirname}/src/assets/images`,
+    //   },
+    // },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `blogPosts`,
+    //     path: `${__dirname}/content/blog`,
+    //   },
+    // },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        "excerpt_separator": `<!-- end -->`,
+        plugins: [
+          {
+            resolve: `gatsby-remark-embedder`
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 800
+            }
+          }
+        ]
+      }
+    },
+    // `gatsby-transformer-sharp`,
+    // `gatsby-plugin-sharp`
+  ],
+}
