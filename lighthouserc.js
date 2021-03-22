dff382c52e642c55abf3ac7f1588f64e93d95470
David Demaree <david@demaree.me>
2021-03-22 19:49:05 -0400

    Remove some dead content

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,15 +1,5 @@
 module.exports = {
   ci: {
-    // collect: {
-    //   url: [
-    //     "http://localhost/index.html",
-    //     "http://localhost/__style-guide/index.html",
-    //     "http://localhost/p/2020-in-review/index.html"
-    //   ],
-    // },
-    // assert: {
-    //   "preset": "lighthouse:no-pwa",
-    // }
     upload: {
       target: 'temporary-public-storage',
     },
