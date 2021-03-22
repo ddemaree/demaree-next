bfbeb3cada281e48ce01213ffc12a71b0a15ff0a
David Demaree <david@demaree.me>
2021-03-22 13:48:39 -0400

    Fix LHCI config

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,10 +1,12 @@
 module.exports = {
   ci: {
-    url: [
-      "http://localhost/index.html",
-      "http://localhost/__style-guide/index.html",
-      "http://localhost/p/2020-in-review/index.html"
-    ],
+    collect: {
+      url: [
+        "http://localhost/index.html",
+        "http://localhost/__style-guide/index.html",
+        "http://localhost/p/2020-in-review/index.html"
+      ],
+    },
     upload: {
       target: 'temporary-public-storage',
     },
