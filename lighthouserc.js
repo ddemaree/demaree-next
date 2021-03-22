7e85b4f5824984b8218e09d53334f863cbb2700c
David Demaree <david@demaree.me>
2021-03-22 14:13:32 -0400

    Disable LHCI URLs hash for now

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,14 +1,17 @@
 module.exports = {
   ci: {
-    collect: {
-      url: [
-        "http://localhost/index.html",
-        "http://localhost/__style-guide/index.html",
-        "http://localhost/p/2020-in-review/index.html"
-      ],
-    },
+    // collect: {
+    //   url: [
+    //     "http://localhost/index.html",
+    //     "http://localhost/__style-guide/index.html",
+    //     "http://localhost/p/2020-in-review/index.html"
+    //   ],
+    // },
     upload: {
       target: 'temporary-public-storage',
     },
+    assert: {
+      "preset": "lighthouse:recommended",
+    }
   },
 };
