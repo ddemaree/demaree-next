0f4bf43d91a59f139f0948b2f43ed1726db10139
David Demaree <david@demaree.me>
2021-03-22 20:29:01 -0400

    Try tweaking LH settings

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,11 @@
 module.exports = {
   ci: {
+    collect: {
+      maxAutodiscoverUrls: 8,
+      settings: {
+        "throttling.cpuSlowdownMultiplier": 2,
+      }
+    },
     upload: {
       target: 'temporary-public-storage',
     },
