d5ae3e4d34bc6ac52a4d26f7576c543f5be9115b
David Demaree <david@demaree.me>
2021-05-29 13:37:59 -0400

    WIP / bail point

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,4 @@
-const _ = require('lodash')
 const defaultTheme = require('tailwindcss/defaultTheme');
-const plugin = require('tailwindcss/plugin')
 
 module.exports = {
 	darkMode: 'media',
@@ -21,74 +19,7 @@ module.exports = {
 		}
 	},
 	plugins: [
-		// Add support for utility-based theming
-		plugin(({ addUtilities, addBase, theme }) => {
-			const propKeys = {
-				'surface': '--color-background',
-				'ink': '--color-ink',
-				'inkBold': '--color-ink-strong',
-				'inkMedium': '--color-ink-medium',
-				'inkLight': '--color-ink-light',
-				'accent': '--color-accent'
-			}
-
-			const themeColors = theme('colors')
-
-			const getInkUtils = (colors, utilsObj = {}, prefix = '') => {
-				const colorPairs = Object.entries(colors)
-
-				colorPairs.forEach(([key, values]) => {
-					if(typeof values === 'string') {
-						_.forIn(propKeys, (customProperty, propKey) => {
-							utilsObj[`.dd-${propKey}${prefix}-${key}`] = {
-								[customProperty]: values
-							}
-						})
-					} else if(typeof values === 'object') {
-						getInkUtils(values, utilsObj, `-${key}`)
-					}
-				})
-
-				return utilsObj
-			}
-
-			const inkUtils = getInkUtils(themeColors)
-			
-			// Now add the text and background utilities for each color
-			_.forIn(propKeys, (customProperty, propKey) => {
-				inkUtils[`.text-${propKey}`] = {
-					color: `var(${customProperty})`
-				}
-				inkUtils[`.bg-${propKey}`] = {
-					backgroundColor: `var(${customProperty})`
-				}
-				inkUtils[`.border-${propKey}`] = {
-					borderColor: `var(${customProperty})`
-				}
-			})
-
-			addUtilities(inkUtils, ['dark', 'hover'])
-
-			addBase({
-				':root': {
-					'background-color': 'var(--color-background)',
-					'--color-ink': theme('colors.gray.800'),
-					'--color-ink-strong': theme('colors.black'),
-					'--color-ink-medium': theme('colors.gray.500'),
-					'--color-ink-light': theme('colors.gray.100'),
-					'--color-background': theme('colors.white'),
-					'--color-accent': theme('colors.red.600'),
-					'@media (prefers-color-scheme: dark)': {
-						'--color-ink': theme('colors.gray.300'),
-						'--color-ink-strong': theme('colors.white'),
-						'--color-ink-medium': theme('colors.gray.400'),
-						'--color-ink-light': theme('colors.gray.700'),
-						'--color-background': theme('colors.black'),
-						'--color-accent': theme('colors.red.400'),
-					}
-				}
-			})
-		})
+		require('@ddemaree/dynamic-ink')
 	],
 	theme: {
 		screens: {
@@ -107,7 +38,6 @@ module.exports = {
 			fontFamily: {
 				sans: ['soehne-web', ...defaultTheme.fontFamily.sans],
 				'sans-display': ['soehne-breit-web', 'soehne-web', ...defaultTheme.fontFamily.sans],
-				'sans-schmal': ['soehne-schmal-web', ...defaultTheme.fontFamily.sans],
 				serif: ['ivar-text', ...defaultTheme.fontFamily.serif],
 				'roslindale': ['Roslindale Variable', 'ivar-text', ...defaultTheme.fontFamily.serif],
 				'roslindale-2': ['"Roslindale Variable 2"', 'ivar-text', ...defaultTheme.fontFamily.serif],
